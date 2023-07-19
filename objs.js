7c68c06cf95bbbf9dfc737345197e9d1f1ee9a11
lilyGevorgyan <lilya.gevorgyan1@tumo.org>
2023-07-19 17:56:29 +0400

    adding pics and objs

--- objs.js
+++ objs.js
@@ -5,7 +5,62 @@ const animals = [
     },
     {
         "pic": "images/cat.png",
-        "name":"Cat"
-    }
-    
+        "name": "Cat"
+    },
+    {
+        "pic": "images/cheetah.png",
+        "name": "Cheetah"
+    },
+    {
+        "pic": "images/cow.png",
+        "name": "Cow"
+    },
+    {
+        "pic": "images/dog.png",
+        "name": "Dog"
+    },
+    {
+        "pic": "images/duck.png",
+        "name": "Duck"
+    },
+    {
+        "pic": "images/flamingo.png",
+        "name": "Flamingo"
+    },
+    {
+        "pic": "images/giraffe.png",
+        "name": "Giraffe"
+    },
+    {
+        "pic": "images/goose.png",
+        "name": "Goose"
+    },
+    {
+        "pic": "images/hippo.png",
+        "name": "Hippo"
+    },
+    {
+        "pic": "images/owl.png",
+        "name": "Owl"
+    },
+    {
+        "pic": "images/panda.png",
+        "name": "Panda"
+    },
+    {
+        "pic": "images/redPanda.png",
+        "name": "Red Panda"
+    },
+    {
+        "pic": "images/rhinocerous.png",
+        "name": "Rhinocerous"
+    },
+    {
+        "pic": "images/.png",
+        "name": ""
+    },
+    {
+        "pic": "images/.png",
+        "name": ""
+    },
 ]
