01ac446bf1f101e317a1dfc40a23dd95117afe05
lilyGevorgyan <lilya.gevorgyan1@tumo.org>
2023-07-19 16:54:17 +0400

    My first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+let seconds = 25;
+let correctAnswer = 0
+let incorrectAnswer = 0
+
+function getElement(id) {
+  return document.getElementById(id);
+}
+
+function timer() {
+setTimeout(finish, seconds * 1000)
+  getElement("time").innerHTML = seconds;
+  let countdown = setInterval(function () {
+    seconds--;
+    getElement("time").textContent = seconds;
+    if (seconds <= 0) {
+      clearInterval(countdown);
+    }
+    if(seconds > 10){
+        getElement("time").style.color = "green"
+    }
+    if (seconds === 10){
+      getElement("time").style.color = "orange";
+    } 
+    if (seconds === 5){
+      getElement("time").style.color = "#ff0000";
+    } 
+  }, 1000);
+}
+
+function check() {
+    let input;
+    try {
+        input = document.querySelector('input[name = "option"]:checked').value
+    } catch{
+        return;
+    }if (input === "Lion"){
+        correctAnswer++
+        getElement("score").innerHTML = correctAnswer;
+    } else {
+        incorrectAnswer++
+    }
+    clearInterval(checkInterval)
+}
+
+function finish(){
+    clearInterval(checkInterval);
+    let percentage = (correctAnswer / (correctAnswer + incorrectAnswer)) * 100;
+    getElement("alertaccuracy").innerHTML = `Ձեր արդյունքն է ${percentage}%`
+}
+
+let checkInterval = setInterval(check, 50)
+
+timer();
---
+++ objs.js
@@ -0,0 +1,11 @@
+const animals = [
+    {
+        "pic": "images/lion.png",
+        "name": "Lion"
+    },
+    {
+        "pic": "images/cat.png",
+        "name":"Cat"
+    }
+    
+]
